e3f91cf714e94e73dc809b88feabfab6245e0933
Sushant dubey <sushantdubey7607@gmail.com>
2026-06-21 16:14:35 +0530

    vercel commit update

--- frontend/PerplexityFrontend/src/configuration/config.ts
+++ frontend/PerplexityFrontend/src/configuration/config.ts
@@ -1,8 +1,8 @@
-
-
 const BACK_END_PORT = 3000;
-
+const BACK_END_URL = import.meta.env.VITE_BACKEND_URL;
 export const config = {
   BACK_END_PORT,
-  BACKEND_URL: `http://localhost:${BACK_END_PORT}`,
+  //BACKEND_URL: `http://localhost:${BACK_END_PORT}`, //for localhost change to this
+  BACKEND_URL: BACK_END_URL , 
+
 } as const;
